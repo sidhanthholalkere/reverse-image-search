693a0b96a1121ece0f77b7dba2ed08695c6ecc2d
rebecca0323 <rebeccaazhu@gmail.com>
2020-07-23 14:49:14 -0400

    update cos_sim

--- cos_sim.py
+++ cos_sim.py
@@ -16,27 +16,3 @@ def cosine_dist(d1, d2):
     
     """
     return 1 - (np.dot(d1, d2)) / (np.linalg.norm(d1)* np.linalg.norm(d2))
-
-
-import io
-import requests
-from PIL import Image
-
-__all__ = ["download_image"]
-
-
-def download_image(img_url: str) -> Image:
-    """ Fetches an image from the web.
-
-    Parameters
-    ----------
-    img_url : string
-        The url of the image to fetch.
-
-    Returns
-    -------
-    PIL.Image
-        The image."""
-
-    response = requests.get(img_url)
-    return Image.open(io.BytesIO(response.content))
