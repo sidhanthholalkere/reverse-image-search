576f8a68d0db391db73e0422e9bf8e62cd290aa0
CrAZYGreenAlpaca <39609875+CrAZYGreenAlpaca@users.noreply.github.com>
2020-07-24 20:17:23 -0700

    some changes to model Ex

--- TrainModelEx.py
+++ TrainModelEx.py
@@ -1,13 +1,14 @@
 from Model import Model
 from Model import train
 from Model import accuracy
-
+from extract_triplets import all_triplets
 
 from noggin import create_plot
 
+#only for jupyter notebook
 plotter, fig, ax = create_plot(metrics=["loss", "accuracy"])
 
-
+#getting model, model params, data
 learning_rate=0.1
 model = Model(512, 50)
 num_epochs = 1
@@ -16,5 +17,7 @@ margin = 0.1
 path = r'data\resnet18_features.pkl'
 triplets = all_triplets(path)
 
-
+#training the model!!
 train(model, num_epochs, margin, triplets, learning_rate=learning_rate, batch_size=batch_size)
+
+
